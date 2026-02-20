2a947faba7de2fee4d4782e953d7d66761042d6f
zhangbaio <50943373+zhangbaio@users.noreply.github.com>
2026-02-20 23:41:40 +0800

    删除无效类引用

--- src/main/java/com/example/musicwebdav/common/util/JwtUtil.java
+++ src/main/java/com/example/musicwebdav/common/util/JwtUtil.java
@@ -1,11 +1,13 @@
 package com.example.musicwebdav.common.util;
 
+
+import java.security.Key;
+import java.util.Date;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import java.security.Key;
-import java.util.Date;
 import org.springframework.stereotype.Component;
 
 @Component
